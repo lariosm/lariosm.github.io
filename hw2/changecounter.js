b63f7bc2dfc5921c217ce4c6c5fd848e0182d302
Manuel Larios <mlarios12@wou.edu>
2018-10-07 18:13:37 -0700

    Included jQuery functions and HTML table-printing function

--- hw2/changecounter.js
+++ hw2/changecounter.js
@@ -1,5 +1,7 @@
+/* Amount of money to "insert" */
 var credit = 0;
 
+/* Number of each bill dispensed */
 var hundreds = 0;
 var fifties = 0;
 var twenties = 0;
@@ -8,9 +10,20 @@ var fives = 0;
 var ones = 0;
 
 function main() {
-  /* Nothing here yet */
+  $('#submit').on('click', function() {
+    credit = financial(document.getElementById('input-amt').value);
+    dispenseChange();
+    printChange();
+    printTable();
+  });
 }
 
+/* Rounds any decimal values to nearest whole number */
+function financial(x) {
+  return Number.parseFloat(x).toFixed(0);
+}
+
+/* Resets number of bills dispensed */
 function reset() {
   credit = 0;
   hundreds = 0;
@@ -23,27 +36,28 @@ function reset() {
 
 function dispenseChange() {
   while(credit != 0) {
-    if(credit % 100 === 0) {
+    console.log("looping");
+    if($('input[type=checkbox][name="hundred"]').is(':checked') && credit % 100 === 0) {
       credit -= 100;
       hundreds++;
     }
-    else if(credit % 50 === 0) {
+    else if($('input[type=checkbox][name="fifty"]').is(':checked') && credit % 50 === 0) {
       credit -= 50;
       fifties++;
     }
-    else if(credit % 20 === 0) {
+    else if($('input[type=checkbox][name="twenty"]').is(':checked') && credit % 20 === 0) {
       credit -= 20;
       twenties++;
     }
-    else if(credit % 10 === 0) {
+    else if($('input[type=checkbox][name="ten"]').is(':checked') && credit % 10 === 0) {
       credit -= 10;
       tens++;
     }
-    else if(credit % 5 === 0) {
+    else if($('input[type=checkbox][name="five"]').is(':checked') && credit % 5 === 0) {
       credit -= 5;
       fives++;
     }
-    else if(credit % 1 === 0) {
+    else if($('input[type=checkbox][name="one"]').is(':checked') && credit % 1 === 0) {
       credit--;
       ones++;
     }
@@ -60,7 +74,30 @@ function printChange() {
 }
 
 function printTable() {
-  /* Nothing here yet */
+  $('#table-change').append("<div class='print-table'>\
+    <h4>Your change is as follows:</h4>\
+    <table>\
+      <tr>\
+        <th># of $100 Bills</th>\
+        <th># of $50 Bills</th>\
+        <th># of $20 Bills</th>\
+        <th># of $10 Bills</th>\
+        <th># of $5 Bills</th>\
+        <th># of $1 Bills</th>\
+      </tr>\
+      <tr>\
+        <td>" + hundreds + "</td>\
+        <td>" + fifties + "</td>\
+        <td>" + twenties + "</td>\
+        <td>" + tens + "</td>\
+        <td>" + fives + "</td>\
+        <td>" + ones + "</td>\
+      </tr>\
+    </table>\
+    <br>\
+    <button id='clear-data'>Start over</button>\
+    <label>-- Clears table and resets the program</label>\
+  </div>");
 }
 
 $(document).ready(main);
