1f09b468a912f73b40ed99f40e86a6a6fb0e5ee2
Manuel Larios <mlarios12@wou.edu>
2018-10-18 21:43:18 -0700

    Created color controller and web page

---
+++ hw4/webapp/webapp/Controllers/ColorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webapp.Controllers
+{
+    public class ColorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string firstColor, string secondColor)
+        {
+            return View();
+        }
+    }
+}
