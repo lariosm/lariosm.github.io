a099eb1264e5168b2f8e1e59767748fda9bafe21
Manuel Larios <mlarios12@wou.edu>
2018-10-21 20:56:20 -0700

    added more comments where needed

--- hw4/webapp/webapp/Controllers/HomeController.cs
+++ hw4/webapp/webapp/Controllers/HomeController.cs
@@ -7,34 +7,31 @@ using System.Web.Mvc;
 
 namespace webapp.Controllers
 {
+    /// <summary>
+    /// Controller for "Home" pages
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Displays the index.cshtml page
+        /// </summary>
+        /// <returns>view of index.cshtml</returns>
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
+        /// <summary>
+        /// GET method for MileConverter.cshtml page
+        /// </summary>
+        /// <returns>view of MileConverter.cshtml</returns>
         [HttpGet]
         public ActionResult MileConverter()
         {
             double miles;
             double metricResult = 0; //where metric conversion values will be stored
             string unit = Request.QueryString["unitselect"];
-            ViewBag.ValidUnit = false;
+            ViewBag.Show = false;
 
             if(Request.QueryString["btn"] == "Convert") //has "Convert" button been pressed?
             {
@@ -45,40 +42,40 @@ namespace webapp.Controllers
                         if (unit == "millimeters")
                         {
                             metricResult = miles * 1609000;
-                            ViewBag.ValidUnit = true;
+                            ViewBag.Show = true;
                         }
                         else if (unit == "centimeters")
                         {
                             metricResult = miles * 160934.4;
-                            ViewBag.ValidUnit = true;
+                            ViewBag.Show = true;
                         }
                         else if (unit == "meters")
                         {
                             metricResult = miles * 1609.344;
-                            ViewBag.ValidUnit = true;
+                            ViewBag.Show = true;
                         }
                         else if (unit == "kilometers")
                         {
                             metricResult = miles * 1.609;
-                            ViewBag.ValidUnit = true;
+                            ViewBag.Show = true;
                         }
                         else //not a valid unit of measure
                         {
                             ViewBag.Display = "Invalid unit type. Please try again.";
                         }
                     }
-                    else
+                    else //if not, let the user know
                     {
                         ViewBag.Display = "Negative values are not allowed. Please try again.";
                     }
                     
 
-                    if (ViewBag.ValidUnit)
+                    if (ViewBag.Show) //displays computed output to user
                     {
                         ViewBag.Display = miles + " miles is equal to " + metricResult + " " + unit;
                     }
                 }
-                else
+                else //if not, let the user know
                 {
                     ViewBag.Display = "Invalid input. Please try again.";
                 }
