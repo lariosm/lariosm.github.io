5e10de882e74dd26b9bf41ee0cb02c63db7a2052
Manuel Larios <mlarios12@wou.edu>
2018-10-27 14:14:16 -0700

    Added SQL scripts

---
+++ hw5/Apartment/Apartment/App_Data/up.sql
@@ -0,0 +1,14 @@
+﻿-- Tenants table
+CREATE TABLE [dbo].[Tenants]
+(
+	[ID]			INT IDENTITY (1,1)	NOT NULL,
+	[FirstName]		NVARCHAR(64)		NOT NULL,
+	[LastName]	    NVARCHAR(128)		NOT NULL,
+	[PhoneNumber]   NVARCHAR(10)		NOT NULL, 
+    [ApartmentName] NVARCHAR(100)		NOT NULL, 
+    [UnitNumber]	NVARCHAR(2)			NOT NULL, 
+    [Description]	NVARCHAR(500)		NOT NULL, 
+    [Checkbox]		TINYINT				NOT NULL, 
+    [Received]		TIMESTAMP			NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    CONSTRAINT [PK_dbo.Users] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
