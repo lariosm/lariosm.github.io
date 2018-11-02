d6c061b46a0c6b2347180ec76f2707a17f559de6
Manuel Larios <mlarios12@wou.edu>
2018-11-01 23:33:45 -0700

    Created working search bar

--- hw6/WWImporters/WWImporters/Controllers/HomeController.cs
+++ hw6/WWImporters/WWImporters/Controllers/HomeController.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WWImporters.Models;
 
 namespace WWImporters.Controllers
 {
     public class HomeController : Controller
     {
+        private WWIContext db = new WWIContext();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index(string query)
+        {
+            IEnumerable<Person> people = db.People.Where(p => p.FullName.Contains(query));
+            if(people != null)
+            {
+                Debug.WriteLine("I found people");
+                Debug.WriteLine(DateTime.Now);
+                return View(people);
+            }
+            else
+            {
+                ViewBag.Message = "No results found.";
+                return View();
+            }
         }
 
         public ActionResult About()
