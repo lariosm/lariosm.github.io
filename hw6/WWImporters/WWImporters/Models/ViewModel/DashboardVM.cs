0cc9e6eddbd88a99525e9bad8c2d6c5a36670692
Manuel Larios <mlarios12@wou.edu>
2018-11-03 22:59:40 -0700

    Added ViewModel class

---
+++ hw6/WWImporters/WWImporters/Models/ViewModel/DashboardVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WWImporters.Models.ViewModel
+{
+    public class DashboardVM
+    {
+        public Person Person { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public IEnumerable<Invoice> Invoice { get; set; }
+
+        public IEnumerable<InvoiceLine> InvoiceLine { get; set; }
+    }
+}
