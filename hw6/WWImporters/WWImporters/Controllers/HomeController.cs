9df03f01dc4ed9979f92c79bd311a55ebcd36f03
Manuel Larios <mlarios12@wou.edu>
2018-11-04 23:25:32 -0800

    Swapped out Person model with my ViewModel

--- hw6/WWImporters/WWImporters/Controllers/HomeController.cs
+++ hw6/WWImporters/WWImporters/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WWImporters.Models;
+using WWImporters.Models.ViewModel;
 
 namespace WWImporters.Controllers
 {
@@ -13,21 +14,26 @@ namespace WWImporters.Controllers
     {
         private WWIContext db = new WWIContext();
 
+        /// <summary>
+        /// Performs the search based on the keywords passed from search box
+        /// </summary>
+        /// <param name="query">keyword(s) to search</param>
+        /// <returns>A list of Person objects</returns>
         [HttpGet]
         public ActionResult Index(string query)
         {
-            IEnumerable<Person> people = db.People.Where(p => p.FullName.Contains(null));
-            ViewBag.ShowError = false;
-            ViewBag.ResultString = "";
+            IEnumerable<Person> people = db.People.Where(p => p.FullName.Contains(null)); //start out with an empty container
+            ViewBag.ShowError = false; //do not show search error if loading the page for the first time or after a successful search
+            ViewBag.ResultString = ""; //shows empty "result" string if loading the page for the first time or after a failed search
 
-            if(!string.IsNullOrWhiteSpace(query))
+            if(!string.IsNullOrWhiteSpace(query)) //makes sure query is not blank or contains only whitespaces.
             {
-                people = db.People.Where(peopleItem => peopleItem.FullName.Contains(query));
-                if (!people.Any())
+                people = db.People.Where(peopleItem => peopleItem.FullName.Contains(query)); //performs the query
+                if (!people.Any()) //No matches? It's a failed search.
                 {
-                    ViewBag.ShowError = true;
+                    ViewBag.ShowError = true; //display failed search message
                 }
-                else
+                else //successful search
                 {
                     ViewBag.ResultString = "Names matching your search: \"" + query + "\"";
                 }
@@ -39,16 +45,18 @@ namespace WWImporters.Controllers
         // GET: People/Details/5
         public ActionResult Details(int? id)
         {
-            if (id == null)
+            DashboardVM vm = new DashboardVM();
+
+            if (id == null) //Non-existant Person ID?
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); //Send out a 400 bad request error.
             }
-            Person person = db.People.Find(id);
-            if (person == null)
+            vm.Person = db.People.Find(id);
+            if (vm.Person == null) //Non-existant Person object?
             {
-                return HttpNotFound();
+                return HttpNotFound(); //Send out a 404 not found error.
             }
-            return View(person);
+            return View(vm);
         }
     }
 }
