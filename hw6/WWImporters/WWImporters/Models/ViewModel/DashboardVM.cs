1952395c46bb87b4a7204ffe817ff226dea4f062
Manuel Larios <mlarios12@wou.edu>
2018-11-05 21:21:30 -0800

    Added license attribution statement for icon used

--- hw6/WWImporters/WWImporters/Models/ViewModel/DashboardVM.cs
+++ hw6/WWImporters/WWImporters/Models/ViewModel/DashboardVM.cs
@@ -5,14 +5,29 @@ using System.Web;
 
 namespace WWImporters.Models.ViewModel
 {
+    /// <summary>
+    /// Allows multiple objects to be returned to the view
+    /// </summary>
     public class DashboardVM
     {
+        /// <summary>
+        /// Person object
+        /// </summary>
         public Person Person { get; set; }
 
+        /// <summary>
+        /// Customer object
+        /// </summary>
         public Customer Customer { get; set; }
 
+        /// <summary>
+        /// List of invoice objects
+        /// </summary>
         public IEnumerable<Invoice> Invoice { get; set; }
 
+        /// <summary>
+        /// List of InvoiceLine objects
+        /// </summary>
         public IEnumerable<InvoiceLine> InvoiceLine { get; set; }
     }
 }
