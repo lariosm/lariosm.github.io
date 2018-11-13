4311fac5791ed1608709c701c111576b1bd1b076
Manuel Larios <mlarios12@wou.edu>
2018-11-13 12:12:15 -0800

    Modified up.sql script

--- hw8/AuctionHouse/AuctionHouse/App_Data/up.sql
+++ hw8/AuctionHouse/AuctionHouse/App_Data/up.sql
@@ -12,16 +12,36 @@ CREATE TABLE [dbo].[Buyers]
 
 CREATE TABLE [dbo].[Items]
 (
-	[ItemID]		INT IDENTITY (1,1)						NOT NULL	PRIMARY KEY,
-	[Name]			NVARCHAR(100)							NOT NULL,
-	[Description]	NVARCHAR(MAX)							NOT NULL,
-	[Seller]		INT FOREIGN KEY REFERENCES Sellers(ID)	NOT NULL
+	[ID]			INT IDENTITY (1001,1)						NOT NULL	PRIMARY KEY,
+	[Name]			NVARCHAR(100)								NOT NULL,
+	[Description]	NVARCHAR(MAX)								NOT NULL,
+	[Seller]		INT FOREIGN KEY REFERENCES Sellers(ID)		NOT NULL
 );
 
 CREATE TABLE [dbo].[Bids]
 (
-	[ItemID]		INT FOREIGN KEY REFERENCES Items(ItemID)	NOT NULL,
-	[Bidder]		INT FOREIGN KEY REFERENCES Buyers(ID)		NOT NULL,
+	[ID]			INT IDENTITY (1,1)							NOT NULL,
+	[ItemID]		INT FOREIGN KEY REFERENCES Items(ID)		NOT NULL,
+	[BuyerID]		INT FOREIGN KEY REFERENCES Buyers(ID)		NOT NULL,
 	[Price]			NVARCHAR(20)								NOT NULL,
 	[TimeStamp]		DATETIME									NOT NULL,
 );
+
+INSERT INTO [dbo].[Sellers] (Name) VALUES
+	('Gayle Hardy'),
+	('Lyle Banks'),
+	('Pearl Greene');
+
+INSERT INTO [dbo].[Buyers] (Name) VALUES
+	('Jane Stone'),
+	('Tom McMasters'),
+	('Otto Vanderwall');
+
+INSERT INTO [dbo].[Items] (Name) VALUES
+	('Abraham Lincoln Hammer', 'A bench mallet fashioned from a broken rail-splitting maul in 1829 and owned by Abraham Lincoln'),
+	('Albert Einsteins Telescope', 'A brass telescope owned by Albert Einstein in Germany, circa 1927'),
+	('Bob Dylan Love Poems', 'Five versions of an original unpublished, handwritten, love poem by Bob Dylan');
+
+INSERT INTO [dbo].[Bids] (ItemID, BidID, Price, TimeStamp) VALUES
+	(1001, 1, 250000, '12/04/2017 09:04:22'),
+	(1003, 3, 95000 ,'12/04/2017 08:44:03');
