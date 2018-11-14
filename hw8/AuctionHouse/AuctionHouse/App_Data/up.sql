5f76c5a24b1d77d434b2c7af0029d62802ed15c8
Manuel Larios <mlarios12@wou.edu>
2018-11-14 01:40:29 -0800

    Created models, controller and views

--- hw8/AuctionHouse/AuctionHouse/App_Data/up.sql
+++ hw8/AuctionHouse/AuctionHouse/App_Data/up.sql
@@ -20,7 +20,7 @@ CREATE TABLE [dbo].[Items]
 
 CREATE TABLE [dbo].[Bids]
 (
-	[ID]			INT IDENTITY (1,1)							NOT NULL,
+	[ID]			INT IDENTITY (1,1)							NOT NULL	PRIMARY KEY,
 	[ItemID]		INT FOREIGN KEY REFERENCES Items(ID)		NOT NULL,
 	[BuyerID]		INT FOREIGN KEY REFERENCES Buyers(ID)		NOT NULL,
 	[Price]			NVARCHAR(20)								NOT NULL,
@@ -37,11 +37,11 @@ INSERT INTO [dbo].[Buyers] (Name) VALUES
 	('Tom McMasters'),
 	('Otto Vanderwall');
 
-INSERT INTO [dbo].[Items] (Name) VALUES
-	('Abraham Lincoln Hammer', 'A bench mallet fashioned from a broken rail-splitting maul in 1829 and owned by Abraham Lincoln'),
-	('Albert Einsteins Telescope', 'A brass telescope owned by Albert Einstein in Germany, circa 1927'),
-	('Bob Dylan Love Poems', 'Five versions of an original unpublished, handwritten, love poem by Bob Dylan');
+INSERT INTO [dbo].[Items] (Name, Description, Seller) VALUES
+	('Abraham Lincoln Hammer', 'A bench mallet fashioned from a broken rail-splitting maul in 1829 and owned by Abraham Lincoln', 3),
+	('Albert Einsteins Telescope', 'A brass telescope owned by Albert Einstein in Germany, circa 1927', 1),
+	('Bob Dylan Love Poems', 'Five versions of an original unpublished, handwritten, love poem by Bob Dylan', 2);
 
-INSERT INTO [dbo].[Bids] (ItemID, BidID, Price, TimeStamp) VALUES
+INSERT INTO [dbo].[Bids] (ItemID, BuyerID, Price, TimeStamp) VALUES
 	(1001, 1, 250000, '12/04/2017 09:04:22'),
 	(1003, 3, 95000 ,'12/04/2017 08:44:03');
---
+++ hw8/AuctionHouse/AuctionHouse/Models/Bid.cs
@@ -0,0 +1,27 @@
+namespace AuctionHouse.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Bid
+    {
+        public int ID { get; set; }
+
+        public int ItemID { get; set; }
+
+        public int BuyerID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Price { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public virtual Buyer Buyer { get; set; }
+
+        public virtual Item Item { get; set; }
+    }
+}
