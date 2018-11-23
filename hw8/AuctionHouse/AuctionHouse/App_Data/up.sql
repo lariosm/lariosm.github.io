c3ab5d91a610ef4bd1ef6f1de6fac3db46b2d51f
Manuel Larios <mlarios12@wou.edu>
2018-11-23 12:38:28 -0800

    Commented code where needed

--- hw8/AuctionHouse/AuctionHouse/App_Data/up.sql
+++ hw8/AuctionHouse/AuctionHouse/App_Data/up.sql
@@ -1,15 +1,18 @@
+--Sellers table
 CREATE TABLE [dbo].[Sellers]
 (
 	[ID]	INT IDENTITY (1,1)	NOT NULL	PRIMARY KEY,
 	[Name]	NVARCHAR(30)		NOT NULL
 );
 
+--Buyers table
 CREATE TABLE [dbo].[Buyers]
 (
 	[ID]	INT IDENTITY (1,1)	NOT NULL	PRIMARY KEY,
 	[Name]	NVARCHAR(30)		NOT NULL
 );
 
+--Items table
 CREATE TABLE [dbo].[Items]
 (
 	[ID]			INT IDENTITY (1001,1)						NOT NULL	PRIMARY KEY,
@@ -18,6 +21,7 @@ CREATE TABLE [dbo].[Items]
 	[SellerID]		INT FOREIGN KEY REFERENCES Sellers(ID)		NOT NULL
 );
 
+--Bids table
 CREATE TABLE [dbo].[Bids]
 (
 	[ID]			INT IDENTITY (1,1)							NOT NULL	PRIMARY KEY,
@@ -27,6 +31,7 @@ CREATE TABLE [dbo].[Bids]
 	[TimeStamp]		DATETIME									NOT NULL,
 );
 
+--Seller seed data
 INSERT INTO [dbo].[Sellers] (Name) VALUES
 	('Gayle Hardy'),
 	('Lyle Banks'),
@@ -34,6 +39,7 @@ INSERT INTO [dbo].[Sellers] (Name) VALUES
 	('Jen Brooks'),
 	('Todd Young');
 
+--Buyer seed data
 INSERT INTO [dbo].[Buyers] (Name) VALUES
 	('Jane Stone'),
 	('Tom McMasters'),
@@ -41,11 +47,13 @@ INSERT INTO [dbo].[Buyers] (Name) VALUES
 	('John Smith'),
 	('Emma Jones');
 
+--Item seed data
 INSERT INTO [dbo].[Items] (Name, Description, SellerID) VALUES
 	('Abraham Lincoln Hammer', 'A bench mallet fashioned from a broken rail-splitting maul in 1829 and owned by Abraham Lincoln', 3),
 	('Albert Einsteins Telescope', 'A brass telescope owned by Albert Einstein in Germany, circa 1927', 1),
 	('Bob Dylan Love Poems', 'Five versions of an original unpublished, handwritten, love poem by Bob Dylan', 2);
 
+--Bids seed data
 INSERT INTO [dbo].[Bids] (ItemID, BuyerID, Price, TimeStamp) VALUES
 	(1001, 1, 250000, '11/04/2018 09:04:22'),
 	(1003, 3, 95000 ,'11/04/2018 08:44:03'),
